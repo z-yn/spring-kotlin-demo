b33f477b865aa7dd9fceb5586bb5c46a6e1430c6
邹亚男 <zouyanan@eccom.com.cn>
2023-08-16 20:11:29 +0800

    add TEST

--- src/test/kotlin/com/example/springkotlin/StructuredConcurrencyTest.kt
+++ src/test/kotlin/com/example/springkotlin/StructuredConcurrencyTest.kt
@@ -1,48 +1,55 @@
 package com.example.springkotlin
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import java.util.concurrent.StructuredTaskScope
+
 
 //https://openjdk.org/jeps/453
 internal class StructuredConcurrencyTest {
-//    @Test
-//    fun awaitAll() {
-//        try {
-//            runBlocking {
-//                val a = async {
-//                    delay(2000)
-//                    if (Math.random() > 0) {
-//                        throw RuntimeException("")
-//                    }
-//                    return@async 1
-//                }
-//
-//                launch {
-//                    while (true) {
-//                        println("I am B")
-//                        delay(1000)
-//                    }
-//                }
-//                val await = a.await()
-//                await
-//            }
-//        } catch (e: Exception) {
-//            println(e.stackTrace)
-//        }
-//        Thread.sleep(10000)
-//    }
-//
-//    @Throws(Exception::class)
-//    @Test
-//    fun test() {
-//        val shutdownOnFailureScope = StructuredTaskScope.ShutdownOnFailure()
-//        val userName = shutdownOnFailureScope.fork {
-//            Thread.sleep(100)
-//            "userName"
-//        }
-//        val orderId = shutdownOnFailureScope.fork {
-//            Thread.sleep(100)
-//            1
-//        }
-//        shutdownOnFailureScope.join().throwIfFailed()
-//        println("userName is ${userName.get()}, orderId is ${orderId.get()}")
-//    }
+    @Test
+    fun awaitAll() {
+        try {
+            runBlocking {
+                val a = async {
+                    delay(2000)
+                    if (Math.random() > 0) {
+                        throw RuntimeException("")
+                    }
+                    return@async 1
+                }
+
+                launch {
+                    while (true) {
+                        println("I am B")
+                        delay(1000)
+                    }
+                }
+                val await = a.await()
+                await
+            }
+        } catch (e: Exception) {
+            println(e.stackTrace)
+        }
+        Thread.sleep(10000)
+    }
+
+    @Throws(Exception::class)
+    @Test
+    fun test() {
+        val shutdownOnFailureScope = StructuredTaskScope.ShutdownOnFailure()
+        val userName = shutdownOnFailureScope.fork {
+            Thread.sleep(100)
+            "userName"
+        }
+        val orderId = shutdownOnFailureScope.fork {
+            Thread.sleep(100)
+            1
+        }
+        shutdownOnFailureScope.join().throwIfFailed()
+        println("userName is ${userName.get()}, orderId is ${orderId.get()}")
+    }
 }
