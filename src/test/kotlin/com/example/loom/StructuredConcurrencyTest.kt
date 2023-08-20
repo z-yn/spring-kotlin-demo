7db4e063457c3da7cdb7c0592beb266f044ce736
邹亚男 <zouyanan@eccom.com.cn>
2023-08-21 07:48:04 +0800

    add TEST for StructuredConcurrencyTest

--- src/test/kotlin/com/example/loom/StructuredConcurrencyTest.kt
+++ src/test/kotlin/com/example/loom/StructuredConcurrencyTest.kt
@@ -1,55 +1,64 @@
 package com.example.loom
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import java.util.concurrent.Callable
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Future
 import java.util.concurrent.StructuredTaskScope
+import java.util.concurrent.StructuredTaskScope.ShutdownOnFailure
+import java.util.function.Function
 
 
 //https://openjdk.org/jeps/453
+//错误短熔断
+//取消传播
+//清晰的
+//可观测的
 internal class StructuredConcurrencyTest {
-    @Test
-    fun awaitAll() {
-        try {
-            runBlocking {
-                val a = async {
-                    delay(2000)
-                    if (Math.random() > 0) {
-                        throw RuntimeException("")
-                    }
-                    return@async 1
-                }
 
-                launch {
-                    while (true) {
-                        println("I am B")
-                        delay(1000)
-                    }
-                }
-                val await = a.await()
-                await
+    private fun <T> asFuture(task: Callable<T>): Callable<Future<T>> {
+        return Callable {
+            try {
+                return@Callable CompletableFuture.completedFuture<T>(task.call())
+            } catch (ex: java.lang.Exception) {
+                return@Callable CompletableFuture.failedFuture<T>(ex)
             }
-        } catch (e: Exception) {
-            println(e.stackTrace)
         }
-        Thread.sleep(10000)
+    }
+
+    fun <T> executeAll(tasks: List<Callable<T>>): List<Future<T>> {
+        ShutdownOnFailure().use { scope ->
+            val futures = tasks.stream()
+                .map{ task: Callable<T> ->
+                    asFuture(task)
+                }.map {
+                    scope.fork(it)
+                }
+                .toList()
+            scope.join()
+            return futures.stream().map {
+                it.get()
+            }.toList()
+        }
     }
 
     @Throws(Exception::class)
     @Test
     fun test() {
-        val shutdownOnFailureScope = StructuredTaskScope.ShutdownOnFailure()
-        val userName = shutdownOnFailureScope.fork {
-            Thread.sleep(100)
-            "userName"
-        }
-        val orderId = shutdownOnFailureScope.fork {
-            Thread.sleep(100)
-            1
-        }
-        shutdownOnFailureScope.join().throwIfFailed()
-        println("userName is ${userName.get()}, orderId is ${orderId.get()}")
+     StructuredTaskScope.ShutdownOnFailure().use {
+         val sub1 = it.fork {
+             println("start subTask1")
+             Thread.sleep(100)
+             println("subTask1 throw Exception")
+             throw Exception("subTask1")
+         }
+         val sub2 = it.fork {
+             println("start subTask2")
+             Thread.sleep(1000)
+             println("end subTask2")
+         }
+         it.join().throwIfFailed()
+     }
+
     }
 }
